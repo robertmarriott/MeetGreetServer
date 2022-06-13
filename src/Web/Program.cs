6f2756a5df8147a119c51883cd91bc8613a055d3
Robert Marriott <marriott@telus.net>
2022-06-12 21:37:21 -0700

    Remove DI registration

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -1,17 +1,24 @@
 using Meetings.Infrastructure.Persistence;
-using Microsoft.EntityFrameworkCore;
+// TODO: REMOVE using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<MeetingsDbContext>();
+// TODO: REMOVE builder.Services.AddDbContext<MeetingsContext>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// TODO: Do I need this?
+// using (var scope = app.Services.CreateScope())
+// {
+//     var scopedProvider = scope.ServiceProvider;
+//     var meetingsContext = scopedProvider.GetRequiredService<MeetingsContext>();
+// }
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
